8fd05ae116b4bf7623132b5a1a119e520340ffbb
Meldencio Czarlemagne <mczvc@proton.me>
2024-04-04 17:09:07 +0800

    [0.1.13] (scroll_animation.js)
    + add scroll animation javascript

---
+++ assets/js/scroll_animation.js
@@ -0,0 +1,20 @@
+
+
+let section = document.querySelectorAll('section');
+let menu = document.querySelectorAll('nav a');
+
+window.onscroll = () => {
+    section.forEach(i => {
+        let top = window.scrollY;
+        let offset = i.offsetTop - 150;
+        let height = i.offsetHeight;
+        let id = i.getAttribute('id');
+        if (top >= offset && top < offset + height) {
+            menu.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('nav a[href*=' + id + ']')
+                    .classList.add('active');
+            });
+        }
+    });
+};
