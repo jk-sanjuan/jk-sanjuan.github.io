802bcc163bf373ad546a8300848028df99aa1b62
Johnella Kyle San Juan <czarm827@gmail.com>
2024-06-01 12:21:02 +0800

    [0.1.14.2] Change sizes when screen is small. *+ update tailwind.css

--- assets/js/project.js
+++ assets/js/project.js
@@ -42,10 +42,10 @@ function breadcrumbs(m) {
         scroll_on_top.classList.remove("hide");
         mainnav.classList.add("show");
         mainnav.classList.remove("hide");
-        tbar.style.height = "177px";
+        tbar.style.height = "160px";
     } else {
         logo_menu();
-        tbar.style.height = "102px";
+        tbar.style.height = "82px";
         scroll_on_top.classList.add("hide");
         scroll_on_top.classList.remove("show");
         mainnav.classList.add("hide");
