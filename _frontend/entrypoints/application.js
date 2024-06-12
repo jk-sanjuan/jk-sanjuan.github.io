09553f8a22f17584da65233d7dc06fc3aa6fd83f
Johnella Kyle San Juan <czarm827@gmail.com>
2024-06-12 22:55:55 +0800

    [0.1.14.3] * fix

--- _frontend/entrypoints/application.js
+++ _frontend/entrypoints/application.js
@@ -2,8 +2,17 @@ import 'virtual:windi.css'
 import 'virtual:windi-devtools'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import PrimeVue from 'primevue/config'
+import Menu from "primevue/menu"
+
+const pinia = createPinia()
 
 import ScrollToTop from "~/components/ScrollToTop.vue";
+import AppNavMore from "~/components/AppNavMore.vue";
+
 createApp(ScrollToTop).mount('#scroll-to-top')
+createApp(AppNavMore).component('Menu', Menu).use(PrimeVue).mount('#app-nav-more')
 
 console.log('Vite ⚡️ Ruby')
