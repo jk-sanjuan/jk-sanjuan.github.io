a513f7341b9d4aee05ca4ee7a761b8eaad7bb52d
Johnella Kyle San Juan <czarm827@gmail.com>
2024-06-12 16:13:06 +0800

    [0.1.14.3] + add scrollToTop component

--- _frontend/components/ScrollToTop.vue
+++ _frontend/components/ScrollToTop.vue
@@ -1,14 +1,31 @@
-
 <script setup lang="ts">
-function scrollToTop() {
+function scrollToTop(event: Event) {
     window.scrollTo({top: 0, behavior: 'smooth'});
 }
 </script>
 
 <template>
-  <div id="scroll-on-top"
-       class="sticky-screen [opacity:0.34] hover:[opacity:0.79] cursor-pointer
-              [bottom:70px] [right:53px] [position:fixed] [z-index:100] animate-opacity hide show">
-    <a @click="scrollToTop"><i class="fa fa-arrow-up ![font-size:32px]"></i></a>
-  </div>
+    <div id="scroll-on-top"
+         class="sticky-screen ![opacity:0.34] hover:![opacity:0.79] cursor-pointer
+              [bottom:70px] [right:53px] [position:fixed] [z-index:100] hide">
+        <a @click="scrollToTop"><i class="fa fa-arrow-up ![font-size:32px]"></i></a>
+    </div>
 </template>
+
+<style lang="scss">
+#scroll-on-top {
+    transition: opacity 0.3s ease-in-out, height 0.3s ease-in-out;
+    height: 63px;
+    opacity: 1;
+    display: block !important;
+}
+
+.hide {
+    transition: opacity 0.3s ease-in-out, height 0.3s ease-in-out;
+    height: 0 !important;
+    opacity: 0;
+    overflow: hidden;
+    margin: 0 !important;
+    padding: 0 !important;
+}
+</style>
