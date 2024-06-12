af393d3322a0e2f984317bbe0118f3f61d409b88
Johnella Kyle San Juan <czarm827@gmail.com>
2024-06-12 14:36:10 +0800

    [0.1.14.3] + refactor

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
+import { resolve } from 'path'
 import {defineConfig} from 'vite'
 import WindiCSS from 'vite-plugin-windicss'
 import RubyPlugin from 'vite-plugin-ruby'
+import vue from '@vitejs/plugin-vue'
+import components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
     plugins: [
+        vue(),
         WindiCSS(),
         RubyPlugin(),
+        components({
+            dirs: [resolve(__dirname, '_frontend/components')],
+            resolvers: [
+
+            ]
+        })
     ],
 })
