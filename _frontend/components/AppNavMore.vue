98a7d6c71d9a4706cceff897cc2792f077ccc64b
Johnella Kyle San Juan <czarm827@gmail.com>
2024-06-12 22:58:26 +0800

    [0.1.14.3] * fix + add to tracking

---
+++ _frontend/components/AppNavMore.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+import {onMounted, ref, reactive, watch, computed} from 'vue';
+
+const items = ref<Array<string>>([]);
+const show = ref<boolean>(false)
+
+onMounted(() => {
+    items.value = window.__navMoreMenu;
+    window.__navMore = show
+})
+</script>
+<template>
+    <div :class="{ 'nav-more--show': show }">
+        <Menu :model="items"/>
+    </div>
+</template>
+<style scoped lang="scss">
+.nav-more--show {
+    @apply [display:block];
+}
+</style>
