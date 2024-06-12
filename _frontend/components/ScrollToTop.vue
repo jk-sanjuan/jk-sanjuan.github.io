91b2fac0ab581c438433c11a575d49817d7b9777
Johnella Kyle San Juan <czarm827@gmail.com>
2024-06-12 14:35:10 +0800

    [0.1.14.3] + add to tracking

---
+++ _frontend/components/ScrollToTop.vue
@@ -0,0 +1,14 @@
+
+<script setup lang="ts">
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+</script>
+
+<template>
+  <div id="scroll-on-top"
+       class="sticky-screen [opacity:0.34] hover:[opacity:0.79] cursor-pointer
+              [bottom:70px] [right:53px] [position:fixed] [z-index:100] animate-opacity hide show">
+    <a @click="scrollToTop"><i class="fa fa-arrow-up ![font-size:32px]"></i></a>
+  </div>
+</template>
---
+++ _frontend/entrypoints/application.js
@@ -0,0 +1,12 @@
+import 'virtual:windi.css'
+import 'virtual:windi-devtools'
+
+import { createApp } from 'vue'
+
+import ScrollToTop from "~/components/ScrollToTop.vue";
+createApp(ScrollToTop).mount('#scroll-to-top')
+
+console.log('Vite ⚡️ Ruby')
+
+// Example: Import a stylesheet in <sourceCodeDir>/index.css
+// import '~/index.css'
