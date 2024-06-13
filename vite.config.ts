79b52e0630939f5a9589e7005c53089fe3eca8c1
Johnella Kyle San Juan <czarm827@gmail.com>
2024-06-13 13:49:10 +0800

    [0.1.14.3] *- remove debug mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,27 @@
 import { resolve } from 'path'
 import {defineConfig} from 'vite'
-import WindiCSS from 'vite-plugin-windicss'
+import windicss from 'vite-plugin-windicss'
 import RubyPlugin from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
+    // define: {
+    //     __VUE_PROD_DEVTOOLS__: true
+    // },
     plugins: [
+        vueDevTools({
+            componentInspector: true
+        }),
         vue(),
-        WindiCSS(),
         RubyPlugin(),
         components({
             dirs: [resolve(__dirname, '_frontend/components')],
             resolvers: [
 
             ]
-        })
+        }),
+        windicss(),
     ],
 })
