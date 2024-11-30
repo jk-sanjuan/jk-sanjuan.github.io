e36e66bdcc13ec35f39236a4de60d22bea043025
Johnella Kyle San Juan <czarm827@gmail.com>
2024-12-01 03:23:29 +0800

    [0.1.14.4] refactor to correct spelling, re-style

--- _frontend/entrypoints/application.js
+++ _frontend/entrypoints/application.js
@@ -12,7 +12,12 @@ const pinia = createPinia()
 import ScrollToTop from "~/components/ScrollToTop.vue";
 import AppNavMore from "~/components/AppNavMore.vue";
 
-createApp(ScrollToTop).mount('#scroll-to-top')
-createApp(AppNavMore).component('Menu', Menu).use(PrimeVue).mount('#app-nav-more')
+// Scroll-to-top as App
+createApp(ScrollToTop).mount('#app-scroll-to-top')
+// App-Nav-More as App
+createApp(AppNavMore).
+    component('Menu', Menu).
+    use(PrimeVue).
+    mount('#app-nav-more')
 
 console.log('Vite ⚡️ Ruby')
