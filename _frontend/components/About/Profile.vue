c162343f339cefdd784bd5216d468be146f0d93f
Johnella Kyle San Juan <czarm827@gmail.com>
2025-06-02 17:06:05 +0800

    refactor home as vue app

---
+++ _frontend/components/About/Profile.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <div id="about-page__profile" class=""
+  >
+    <div id="about--profile">
+      <div id="about__profile-bg-layout" class="flex [flex-wrap:wrap] h-[70vh] justify-center items-center">
+        <div id="about--profile-bg"></div>
+        <div class="[flex-basis:100%] [font-family:Play]">
+          <h2 class="text-white bg-purple-700/20 p-2 hover:text-black animate-text duration-1 my-0"
+          >Ube Theme</h2>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
