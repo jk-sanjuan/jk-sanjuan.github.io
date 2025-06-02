2b36d611dfbaf30487916892e40e97535b1b6fbf
Johnella Kyle San Juan <czarm827@gmail.com>
2025-06-02 17:09:59 +0800

    add Team to About components

---
+++ _frontend/components/About/Team.vue
@@ -0,0 +1,38 @@
+<template>
+
+<div id="about-page__team" class="" >
+  <div class="profile-screen h-[319px] max-w-[500px] md:h-[560px]">
+    <div class="profile-navbar">
+      <div class="list animate-pulse">
+        <div class="item">
+          <div class="img"></div>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <div class="item">
+          <div class="img"></div>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <div class="item">
+          <div class="img"></div>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <div class="item">
+          <div class="img"></div>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
