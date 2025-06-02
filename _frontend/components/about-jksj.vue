9aea0e9da61b1ad3154657e119e794d798025dfa
Johnella Kyle San Juan <czarm827@gmail.com>
2025-06-02 17:07:09 +0800

    0.1.17

--- _frontend/components/about-jksj.vue
+++ _frontend/components/about-jksj.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-[100vw] overflow-hidden">
 
-    <div id="about--intro" class="flex flex-col justify-end items-center h-screen space-y-8">
+    <div id="about--intro" class="flex flex-col justify-end items-center h-full space-y-8">
 
       <div class="flex justify-center items-center">
         <div class="w-[33px] h-[33px] md:w-[86px] md:h-[86px] hover:!bg-gray-800" style="background:
@@ -9,24 +9,32 @@
         </div>
 
         <div class="bg-[rgba(255,255,255,0.7)] h-[66px] md:h-[86px] pr-[9px] md:pr-[117px]">
-          <h1 class="ml-2 bg-sky-400 hover:bg-[mediumvioletred]/40 h-[17px] single-title [line-height:1]
-                     md:[font-size:revert] md:h-[20px]"
+          <h1 class="ml-2 bg-sky-400 hover:bg-[mediumvioletred]/40 h-[17px] single-title [line-height:1] md:[font-size:revert] md:h-[20px]"
+          style="transition: background-color ease 200ms;"
           >Johnella Kyle San Juan</h1>
         </div>
       </div>
 
-      <div>
+      <div class="about-links">
         <a href="#profile" class="text-[#a64d00] font-bold"
-         @click.prevent="() => { showPage('profile') }"
+         @click.prevent="() => { clickPage('profile') }"
+         @mouseenter="() => { peekPage('profile'); }"
+         @mouseleave="() => { peekOutPage()}"
         >Profile</a> /
         <a href="#designs" class="text-[#a64d00] font-bold"
-         @click.prevent="showPage('designs')"
+         @click.prevent="clickPage('designs')"
+         @mouseenter="() => { peekPage('designs'); }"
+         @mouseleave="() => { peekOutPage()}"
         >Designs</a> /
         <a href="#nation" class="text-[#a64d00] font-bold"
-         @click.prevent="showPage('nation')"
+         @click.prevent="clickPage('nation')"
+         @mouseenter="() => { peekPage('nation'); }"
+         @mouseleave="() => { peekOutPage()}"
         >Nation</a> /
         <a href="#team" class="text-[#a64d00] font-bold"
-         @click.prevent="showPage('team')"
+         @click.prevent="clickPage('team')"
+         @mouseenter="() => { peekPage('team'); }"
+         @mouseleave="() => { peekOutPage()}"
         >Team</a>
       </div>
     </div>
@@ -45,68 +53,20 @@
               </div>
             </div>
 
-            <div id="about-page__profile" class=""
-            v-if="currentPage === 'profile'">
-              <div id="about--profile">
-                <div id="about__profile-bg-layout" class="flex [flex-wrap:wrap] h-[70vh] justify-center items-center">
-                  <div id="about--profile-bg"></div>
-                  <div class="[flex-basis:100%] [font-family:Play]">
-                    <h2 class="text-white bg-purple-700/20 p-2 hover:text-black animate-text duration-1 my-0"
-                    >Ube Theme</h2>
-                  </div>
-                </div>
+            <component :is="{
+              profile: 'AboutProfile', 
+              designs: 'AboutDesigns',
+              nation: 'AboutNation',
+              team: 'AboutTeam'
+            }[currentPage] "></component>
 
-              </div>
-            </div>
+            <!-- <AboutProfile v-if="currentPage === 'profile'"></AboutProfile> -->
 
-            <div id="about-page__designs" class="flex justify-center items-center"
-            v-if="currentPage === 'designs'">
-              <div class="page--designs-bg [aspect-ratio:1920/1219] w-[70vh] hover:!bg-gray-900" />
-            </div>
+            <!-- <AboutDesigns v-if="currentPage === 'designs'"></AboutDesigns> -->
 
-            <div id="about-page__nation" class="flex justify-center items-center"
-            v-if="currentPage === 'nation'" >
-              <div class="page--nation-bg w-[300px] h-[150px] border border-solid border-[#0000003b]
-                      bg-[#ffffff08] hover:!bg-gray-200" />
-            </div>
+            <!-- <AboutNation v-if="currentPage === 'nation'"></AboutNation> -->
 
-            <div id="about-page__team" class=""
-            v-if="currentPage === 'team'">
-              <div class="profile-screen h-[319px] max-w-[500px] md:h-[560px]">
-                <div class="profile-navbar">
-                  <div class="list animate-pulse">
-                    <div class="item">
-                      <div class="img"></div>
-                      <span></span>
-                      <span></span>
-                      <span></span>
-                    </div>
-
-                    <div class="item">
-                      <div class="img"></div>
-                      <span></span>
-                      <span></span>
-                      <span></span>
-                    </div>
-
-                    <div class="item">
-                      <div class="img"></div>
-                      <span></span>
-                      <span></span>
-                      <span></span>
-                    </div>
-
-                    <div class="item">
-                      <div class="img"></div>
-                      <span></span>
-                      <span></span>
-                      <span></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-            </div>
+            <!-- <AboutTeam v-if="currentPage === 'team'"></AboutTeam> -->
 
           </div>
         </div>
@@ -117,44 +77,92 @@
 </template>
 
 <script setup lang="ts">
-import {nextTick, onMounted, ref} from 'vue';
+import {onMounted, ref} from 'vue';
 
 type Page = 'profile' | 'designs' | 'nation' | 'team'
 const currentPage = ref<Page>('profile');
+const savedPage = ref<Page>();
+
+let showPageTimeout: NodeJS.Timeout;
+
+function getPageSelector(page: Page) {
+  switch (page) {
+    case "profile":
+      return '#profile #about-page__profile';
+    case "designs":
+      return '#designs #about-page__designs';
+    case "nation":
+      return '#nation #about-page__nation';
+    case "team":
+      return '#team #about-page__team';
+    default:
+      return undefined;
+  }
+}
+
+function clickPage(page: Page) {
+  showPage(page);
+  savedPage.value = currentPage.value;
+}
+
+function peekPage(page: Page) {
+  if (savedPage.value === page) return;
+  currentPage.value = page;
+
+  const pageSelector = getPageSelector(page);
+
+  if (pageSelector) {
+
+    showPageTimeout && clearTimeout(showPageTimeout);
+    showPageTimeout = setTimeout( () => {
+      const pageEl = document.querySelector(pageSelector!)!;
+      pageEl.classList.add('peek-in');
+      setTimeout(() => {
+        pageEl.classList.add('peek');
+      }, 1000);
+    }, 200);
+  }
+}
+
+function peekOutPage() {
+  if (savedPage.value === currentPage.value) return;
+  const page = currentPage.value = savedPage.value!;
+
+  const pageSelector = getPageSelector(page);
+
+  if (pageSelector) {
+
+    showPageTimeout && clearTimeout(showPageTimeout);
+    showPageTimeout = setTimeout( () => {
+      const pageEl = document.querySelector(pageSelector!)!;
+      pageEl.classList.remove('peek-in');
+      pageEl.classList.remove('peek');
+
+      showPage(page);
+
+    }, 200);
+
+  }
+}
 
 function showPage(page: Page) {
   currentPage.value = page;
 
-  let elSelector: string | null | undefined = null;
-  switch (page) {
-    case "profile":
-      elSelector = '#profile #about-page__profile';
-      break;
-    case "designs":
-      elSelector = '#designs #about-page__designs';
-      break;
-    case "nation":
-      elSelector = '#nation #about-page__nation';
-      break;
-    case "team":
-      elSelector = '#team #about-page__team';
-      break;
-    default:
-      elSelector = undefined;
-      break;
-  }
-  if (elSelector) {
-    setTimeout( () => {
-        const pageEl = document.querySelector(elSelector!) as unknown as HTMLElement;
-        pageEl.classList.remove('show');
-        setTimeout(() => { pageEl.classList.add('show')} , 150);
+  const pageSelector = getPageSelector(page);
+  
+  if (pageSelector) {
+    showPageTimeout && clearTimeout(showPageTimeout);
+    showPageTimeout = setTimeout( () => {
+      const pageEl = document.querySelector(pageSelector!) as unknown as HTMLElement;
+      pageEl.classList.remove('show');
+      setTimeout(() => { pageEl.classList.add('show')} , 150);
     }, 150);
   }
 
 }
 
 onMounted( () => {
-  showPage('profile');
+  clickPage('profile');
 })
 
 </script>
@@ -179,8 +187,29 @@ onMounted( () => {
 }
 @media (min-height: 683px) {
   #jk-i {
-    background-position: center -132%;
+    background-position: calc(50% + 105px) -132%;
     background-size: 1230px;
+  }
+}
+
+@media (max-width: 860px) {
+  #jk-i {
+    background-position: 140vw -132%;
+    background-size: 1230px;
+  }
+}
+
+@media (max-width: 720px) {
+  #jk-i {
+    background-position: 31% -925%;
+    background-size: 159vw;
+    height: 110vw;
+  }
+}
+
+.about-links {
+  a {
+    color: rgb(255, 202, 156);
   }
 }
 
@@ -199,7 +228,17 @@ onMounted( () => {
   width: 50vh;
   height: 50vh;
   border-radius: 9999px;
+
+  opacity: 0.7;
+
+  transition: opacity ease 1s;
+
 }
+
+#about--profile-bg:hover::after {
+  opacity: 1;
+}
+
 #about__profile-bg-layout h2 {
   transition: transform ease 1000ms;
   transform: translateX(-50vw);
@@ -217,7 +256,7 @@ onMounted( () => {
 
 #about-page__profile, #about-page__designs, #about-page__nation, #about-page__team {
   opacity: 0;
-  left: -320vw;
+  transform: translateX(-320vw);
   position: relative;
   transition: transform 0.5s ease, opacity 700ms;
 }
@@ -227,8 +266,28 @@ onMounted( () => {
 #nation #about-page__nation.show,
 #team #about-page__team.show {
   opacity: 1;
-  transform: translateX(320vw);
+  transform: translateX(0);
   transition-delay: .4s;
+}
+
+#profile #about-page__profile.peek-in,
+#designs #about-page__designs.peek-in,
+#nation #about-page__nation.peek-in,
+#team #about-page__team.peek-in {
+  opacity: 1;
+  transform: translateX(0);
+  rotate: 10deg;
+  scale: 0.8;
+  transform-origin: center;
+  transition: opacity 700ms, rotate ease 1s, scale ease 1s;
+}
+
+#profile #about-page__profile.peek,
+#designs #about-page__designs.peek,
+#nation #about-page__nation.peek,
+#team #about-page__team.peek {
+  scale: 1;
+  rotate: 0deg;
 }
 
 .page--nation-bg {
@@ -236,6 +295,8 @@ onMounted( () => {
   background-repeat: no-repeat;
   background-size: 600px;
   background-position: center center;
+
+  transition: background-color ease 1s;
 }
 
 .page--designs-bg {
@@ -243,6 +304,13 @@ onMounted( () => {
   background-repeat: no-repeat;
   background-size: 70vh;
   background-position: center center;
+
+  border-radius: 999px;
+  opacity: 0.5;
+}
+
+.page--designs-bg:hover {
+  opacity: 1;
 }
 
 div.profile-screen {
@@ -253,5 +321,13 @@ div.profile-screen {
     overflow: hidden;
     position: relative;
     background: #31558a;
+    
+    opacity: 0.8;
+
+    transition: opacity ease 1s;
+
+    &:hover {
+      opacity: 1;
+    }
 }
 </style>
