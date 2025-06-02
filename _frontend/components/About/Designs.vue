7ba1fc8e91c6389a62f6f2519e14d6f2aba44d84
Johnella Kyle San Juan <czarm827@gmail.com>
2025-06-02 17:09:32 +0800

    add Design to About components

---
+++ _frontend/components/About/Designs.vue
@@ -0,0 +1,7 @@
+<template>
+
+  <div id="about-page__designs" class="flex justify-center items-center"
+>
+    <div class="page--designs-bg [aspect-ratio:1920/1219] w-[70vh] hover:!bg-gray-900" />
+  </div>
+</template>
