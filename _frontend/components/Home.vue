cb6dd3e91fdcc4259c5102ccaf32facf87ef5fcf
Johnella Kyle San Juan <czarm827@gmail.com>
2025-06-02 17:10:39 +0800

    refactor Home to vue component as app

---
+++ _frontend/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-2xl sm:[font-size:2.1rem] bg-yellow-600 py-3 sm:py-5 flex min-w-[300px] sm:h-[97px] h-[69px] page-header">
+
+    <img src="../../assets/images/jk/jk-m.png"
+        class="h-[125px] sm:h-[170px] [position:relative] sm:-top-[16px] -top-[9px] hover:bg-white/20"
+        alt="JK-M">
+
+    <span class="ml-4 mt-[15px] tangerine text-[rgb(255,207,92)] !font-bold text-[50px]">Certified <span class="text-red-600 font-normal djvu-serif-bold text-[38px]">*Chic*</span></span>
+
+  </div>
+
+  <div class="block">
+    <div class="bg-sky-400 w-[100vw] h-[100%] lg:[display:inline-block]">
+
+      <img class="w-full max-w-[600px] max-h-[100vw] bg-black hover:! bg-gray-900 [object-fit:cover]"
+          src="../../assets/images/jk/JK-P.png">
+    </div>
+
+  </div>
+</template>
