89b36fbdcae118e93f7c8cccc171eccc8c1d1762
Johnella Kyle San Juan <czarm827@gmail.com>
2025-06-02 17:09:47 +0800

    add Nation to About components

---
+++ _frontend/components/About/Nation.vue
@@ -0,0 +1,7 @@
+<template>
+
+  <div id="about-page__nation" class="flex justify-center items-center" >
+    <div class="page--nation-bg w-[300px] h-[150px] border border-solid border-[#0000003b]
+            bg-[#ffffff08] hover:!bg-gray-200" />
+  </div>
+</template>
